ca513338a92a1f90ea1c84331bdf409d4353bb2d
IsseNisse <isak.vindeln@gmail.com>
2021-03-09 16:59:04 +0100

    har lagt till fungerande undo knapp

--- src/sample/drawController.java
+++ src/sample/drawController.java
@@ -1,17 +1,26 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+
+import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class drawController {
 
     @FXML
     private Canvas canvas;
+    private Stack<Image> savedImages = new Stack<>();
 
     private int size = 10;
 
@@ -23,20 +32,31 @@ public class drawController {
     private String drawFunction = "freeDraw";
 
     public void draw(javafx.scene.input.MouseEvent mouseEvent) {
+        if (savedImages.empty()) {
+            Image snapshot = canvas.snapshot(null, null);
+            savedImages.push(snapshot);
+        }
         GraphicsContext gc = canvas.getGraphicsContext2D();
         double mouseX = mouseEvent.getX();
         double mouseY = mouseEvent.getY();
 
         if (drawFunction == "freeDraw") {
-            freeDraw(gc, mouseX, mouseY);
+            freeDraw(gc, mouseX, mouseY, mouseEvent);
         } else if (drawFunction == "drawLine") {
             drawLine(gc, mouseX, mouseY, mouseEvent);
         }
     }
 
-    public void freeDraw(GraphicsContext gc, double mouseX, double mouseY) {
-        gc.setFill(Color.GREEN);
-        gc.fillRect(mouseX - (size/2), mouseY - (size/2), size, size);
+    public void freeDraw(GraphicsContext gc, double mouseX, double mouseY, MouseEvent mouseEvent) {
+        EventType<? extends MouseEvent> eventType = mouseEvent.getEventType();
+        if (!eventType.getName().equals("MOUSE_RELEASED")) {
+            gc.setFill(Color.GREEN);
+            gc.fillRect(mouseX - (size/2), mouseY - (size/2), size, size);
+        } else {
+            /* save snapshot */
+            Image snapshot = canvas.snapshot(null, null);
+            savedImages.push(snapshot);
+        }
     }
 
     public void drawLine(GraphicsContext gc, double mouseX, double mouseY, MouseEvent mouseEvent) {
@@ -52,10 +72,18 @@ public class drawController {
             anchor2Y = mouseY;
             gc.setLineWidth(size);
             gc.strokeLine(anchor1X, anchor1Y, anchor2X, anchor2Y);
+
+            /* save snapshot */
+            Image snapshot = canvas.snapshot(null, null);
+            savedImages.push(snapshot);
         }
     }
 
     public void undo(ActionEvent actionEvent) {
+        if (!savedImages.empty()) {
+            Image undoImage = savedImages.pop();
+            canvas.getGraphicsContext2D().drawImage(undoImage, 0, 0);
+        }
     }
 
 
